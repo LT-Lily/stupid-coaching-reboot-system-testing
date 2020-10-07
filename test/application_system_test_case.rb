27e0b59e026e28c1c11b169d9c4f2e92f8d6e8cb
Lily Tan <lilyt2995@gmail.com>
2020-10-07 16:28:20 +1100

    first feature test

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,17 @@
+# require "test_helper"
+
+# class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+#   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+# end
+
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  Capybara.register_driver(:headless_chrome) do |app|
+    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome \
+      chromeOptions: { args: %w[headless disable-gpu window-size=1280x760] }
+    Capybara::Selenium::Driver.new app,
+      browser: :chrome, desired_capabilities: capabilities
+  end
+  driven_by :selenium, using: :headless_chrome
 end
