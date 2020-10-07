aa916073e61abd47273afb7e1d6e8357c30a9a80
Lily Tan <lilyt2995@gmail.com>
2020-10-07 16:42:32 +1100

    TEST: mimic user to yield positive response from coach

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -10,4 +10,13 @@ class QuestionsTest < ApplicationSystemTestCase
     visit ask_url
     assert_selector "h1", text: "Ask your coach anything"
   end
+
+  test "saying I am going to work yields a positive response from the coach" do
+    visit ask_url
+    fill_in "question", with: "I am going to work"
+    click_on "Ask"
+
+    assert_text "Great!"
+  end
+
 end
