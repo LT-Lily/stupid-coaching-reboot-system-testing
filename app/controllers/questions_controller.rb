136c9a1c452ceb89181eb21c09831526ec2eb567
Lily Tan <lilyt2995@gmail.com>
2020-10-07 16:01:33 +1100

    Added rails helper to link back to ask page from aswer page.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,10 +6,10 @@ class QuestionsController < ApplicationController
         @question = params[:question]
         if @question == "I am going to work"
         @answer = "Great!"
-    elsif @question.end_with? "?"
+        elsif @question.end_with? "?"
         @answer = "Don't ask me anything."
-    else
+        else
         @answer = "I don't care, get dressed and go to work!"
-    end
+        end
     end
 end
